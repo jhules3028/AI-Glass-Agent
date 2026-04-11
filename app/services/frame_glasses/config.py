57d7c41d6d010cb490d102a952e5c8089dcebfc2
Jhules3028 <equipodeantropologia123@gmail.com>
2026-04-11 15:57:48 -0600

    se agregan variables globales asociadas a los lentes

--- app/services/frame_glasses/config.py
+++ app/services/frame_glasses/config.py
@@ -1,10 +1,29 @@
 # Variables globales asociadas a los lentes.
 
+# Las variables se manejan en las categorias:
+
+
+# 1. Estado
+#   1.1 NOMBRE_LENTES : Posibles nombres de las gafas
+
+# 2. Foto
+
+#   2.1 RUTA_GUARDADO_IMAGENES  :  ruta donde se guardaran las fotos tomadas por los lentes
+#   2.2 AUTOFOCUS_SECONDS  :  Segundos de enfoque del lente
+#   2.3 QUALITY_PHOTO : Calidad de la foto
+
+# 3. Texto
+
+from frame_sdk.camera import  Quality
 # Estado
+
 NOMBRE_LENTES = "frame-glasses" # Poner una lista/diccionarios de diferentes nombres
 
 # Foto
-# Texto
 
 RUTA_GUARDADO_IMAGENES='app/storage/frame-photos'
+AUTOFOCUS_SECONDS=3
+QUALITY_PHOTO=Quality.HIGH
+
+# Texto
 
