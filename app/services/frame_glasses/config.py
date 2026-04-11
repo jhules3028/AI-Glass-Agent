ac4450bfdacdca00f4841d06c934492f6222e576
Jhules3028 <equipodeantropologia123@gmail.com>
2026-04-11 15:51:43 -0600

    se agrega la clase log_event

--- app/services/frame_glasses/config.py
+++ app/services/frame_glasses/config.py
@@ -1,3 +1,10 @@
-# VAriables globales
+# Variables globales asociadas a los lentes.
+
+# Estado
+NOMBRE_LENTES = "frame-glasses" # Poner una lista/diccionarios de diferentes nombres
+
+# Foto
+# Texto
 
 RUTA_GUARDADO_IMAGENES='app/storage/frame-photos'
+
--- app/services/frame_glasses/log_manager.py
+++ app/services/frame_glasses/log_manager.py
@@ -16,6 +16,34 @@
 #       3.2 Cuanto texto se ha mostrado en los lentes
 #       3.3 No se ha podido mostrar un texto
 
+from typing import Literal
+
 import asyncio
 
-#
+class log_event:
+    # En esta clase estara los diferentes tipos de logs asociados a los lentes.
+
+    def __init__(self,
+                    title: str,
+                    description: str,
+                    type: Literal["State", "Photo", "text"],
+                    important: bool):
+        """
+        Aqui podremos agregar los diferentes eventos que necesitemos
+        """
+        self.title=title                      # Titulo en Negritas del mensaje
+        self.type = type                      # tipo de log: Estado, Foto con los lentes, mostrar texto
+        self.description = description        # contenido del log
+        self.important = important            # Es importante lo que vamos a mostrar? Si/No
+
+
+    def Show_log(self):
+        """
+        Aqui vamos a presentar los lgos del sistema en la pagina.
+        """
+        # En este apartado parecera codigo para que puedas mostrar el contenido de los logs.
+        # Las plantiallas para mostrar los logs estan en:
+
+        # app/static/css/frame/logs.css
+        # app/static/html/frame/logs.html
+        # app/static/js/frame/logs.js
