3c31ef2b95994d6ecab9a6c0f81eecf570799464
Jhules3028 <equipodeantropologia123@gmail.com>
2026-04-25 19:06:02 -0600

    Se agrega una funcion para determianr si los lentes estan ocupados

--- app/services/frame_glasses/runtime.py
+++ app/services/frame_glasses/runtime.py
@@ -1,8 +1,24 @@
 import asyncio
 
+# Este archivo es para poder manejar el uso de funciones dentro de los lentes; Estos no pueden realizar multiples operaciones de manera paralela.
+# Modifica su estado a ocupado o desocupado; impidiendo la falla de tareas porqie estos se encuentran ocupados. Probablemente sea necesario agregar un encolamiento de funciones.
+
+from app.services.frame_glasses.log_manager import log_event    # capturar logs asociados a los lentes.
+
 FRAME_LOCK = asyncio.Lock()
 
-async def Is_frame_bussy() -> bool:
-    print(FRAME_LOCK)
+
+async def Is_frame_bussy(show_log: bool) -> bool:
+
+    """
+    if show_log:
+        log_event(
+            title=f"Actualmente las gafas estan siendo usadas por otro proceso.",
+            description=f"",
+            type="Photo",
+            important=False,
+            show_time=True
+        )
+    """
     return FRAME_LOCK.locked()
 
